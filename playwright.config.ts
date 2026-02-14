689f3b4967db873fcdcdec73102e2776521ba1e4
khlq29 <khaliqanwar25@gmail.com>
2026-02-14 19:54:01 +0530

    playwright demo with video

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,12 @@ export default defineConfig({
 
     // Default timeout for actions
     actionTimeout: 10_000,
+
+    // Maximize browser window
+    viewport: null,
+    launchOptions: {
+      args: ['--start-maximized'],
+    },
   },
 
   // Configure projects for major browsers
